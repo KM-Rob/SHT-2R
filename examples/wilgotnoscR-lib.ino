f611021031ef4844eb2b0aa4a8f4f5a134b05ce6
KM-Rob <66571687+KM-Rob@users.noreply.github.com>
2023-01-28 15:03:48 +0100

    description added
    exemplary file modified - description about circuit connections added.

--- examples/wilgotnoscR-lib.ino
+++ examples/wilgotnoscR-lib.ino
@@ -1,7 +1,13 @@
 #include <SYH_2R.h>
 
+//connect sensing resistor between this digital pin
+//and an analog (ANALOG_IN)
 #define RH_PIN 4
+//connect divider resistor between this digital pin
+//and an analog (ANALOG_IN)
 #define RT_PIN 5
+//analog input for voltage measurement
+//(midle point of voltage divider)
 #define ANALOG_IN A0
 // RT_VAL in kOhms
 #define RT_VAL 53
@@ -19,7 +25,10 @@ hum.begin(RH_PIN,RT_PIN,ANALOG_IN,RT_VAL);
 void loop() {
   // put your main code here, to run repeatedly:
 Serial.print("Rezyst: ");
+// ...measure runs polarization and returns resistance of sensor in kOhms
 Serial.print(hum.measure(10));
 Serial.println("kOhm");
-delay(1000);
+delay(900);
+// ...just run polarization without measurement 
+hum.polarize(50);
 }
